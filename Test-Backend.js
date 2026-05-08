39417aa247cda8f2d376adf2649094abb505d941
Dohar Siregar <doharsiregar@Dohars-MacBook-Pro-2.local>
2026-05-08 14:22:06 +0700

    Result Test Backend

---
+++ Test-Backend.js
@@ -0,0 +1,19 @@
+/**
+ * Direction:
+ * Find maximum number of words from given sentences.
+ *
+ * Expected:
+ * 23
+ */
+const sentences = [
+  'Mauris ultricies sed sapien eget malesuada. Suspendisse et aliquet odio, id ultrices erat. Praesent vehicula erat nulla. Aliquam a lorem urna. Donec.',
+  'Duis at tellus et ex bibendum pellentesque sed in nibh. Sed aliquet, diam id mollis facilisis, massa metus accumsan elit, at mattis magna.',
+  'Sed non nibh quam. Pellentesque eget ultrices diam. Aliquam diam justo, consectetur ac dui lobortis, vestibulum bibendum lorem. Sed porta pulvinar.',
+];
+
+function result(sentences) {
+    const result = sentences[0].length + sentences[1].length + sentences[2].length
+    return result
+}
+
+console.log(result(sentences));
